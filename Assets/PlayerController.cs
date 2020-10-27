8277b92f83d7a2b5ebd5273ec086c70eb08dd703
JoannaGaluszka <jo.galuszka@gmail.com>
2020-10-27 22:45:56 +0100

    rusza sie!

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            rigidbody.velocity = new Vector2(-5, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.velocity = new Vector2(5, 0);
+        }
+    }
+
+}
