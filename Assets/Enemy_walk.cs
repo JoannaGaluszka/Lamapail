bd8e9558011b4653059dffbfecaeed5809e43edb
JoannaGaluszka <jo.galuszka@gmail.com>
2020-10-30 01:21:03 +0100

    AI detection(left/rigt), raycast do dopracowania

---
+++ Assets/Enemy_walk.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Enemy_walk : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+
+    private bool movingLeft = true;
+
+    public Transform detection;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        RaycastHit2D ground = Physics2D.Raycast(detection.position, Vector2.down);
+        if(ground.collider == false)
+        {
+            if(movingLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingLeft = true;
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+        public float speed;
+    private float moveInput;
+    private bool ground;
+    public Transform feetPos;
+    public float radius;
+    public LayerMask whatIsGround;
+    public float ForceJump;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+        ground = Physics2D.OverlapCircle(feetPos.position, radius, whatIsGround);
+        if(moveInput > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        } else if(moveInput < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        
+
+        if(ground == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * ForceJump;
+        }
+    }
+}
+
+
