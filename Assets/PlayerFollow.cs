579e637f834dc713bae1bf90d4eb90db990aed98
jonhonesy <arkadiuszjaroszynskimrc@gmail.com>
2020-10-31 16:16:29 +0100

    Follow Camera

---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform Player;
+
+    void FixedUpdate ()
+    {
+        transform.position = new Vector3(Player.position.x, Player.position.y, transform.position.z);
+    }
+}
