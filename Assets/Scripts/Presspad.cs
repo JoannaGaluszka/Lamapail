9f8c6fab8b5b5500e13c8453213fd951c060ad58
jonhonesy <arkadiuszjaroszynskimrc@gmail.com>
2020-10-31 14:11:10 +0100

    Presspad

---
+++ Assets/Scripts/Presspad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Presspad : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay2D()
+    {
+        anim.SetBool("TransitionPad", true);
+    }
+    void OnTriggerExit2D()
+    {
+        anim.SetBool("TransitionPad", false);
+    }
+}
