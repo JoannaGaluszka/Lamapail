a8a00b26b52be22bfe5dffebe6c7ae9321c73a6d
jonhonesy <arkadiuszjaroszynskimrc@gmail.com>
2020-10-31 23:43:34 +0100

    błędy i porażki

---
+++ Assets/ScriptAttack.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float Cooldown;
+    public float startCooldown;
+    public Transform attackPos;
+    public LayerMask whoisenemy;
+    public float attackRange;
+    public int DMG;
+
+    void Update()
+    {
+        if (Cooldown <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whoisenemy);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(DMG);
+                }
+                Cooldown = startCooldown;
+            }
+            else
+            {
+                Cooldown -= Time.deltaTime;
+
+            }
+
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPos, attackRange);
+        }
+    }
+}
--- Assets/Scripts/EnemyWalk.cs
+++ Assets/Scripts/EnemyWalk.cs
@@ -4,29 +4,10 @@ using UnityEngine;
 
 public class EnemyWalk : MonoBehaviour
 {
-    public float speed;
-    public float distance;
-    private bool movingLeft = true;
-    public Transform detection;
+    
 
     private void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-
-        RaycastHit2D ground = Physics2D.Raycast(detection.position, Vector2.down, distance);
-        if (ground.collider == false)
-        {
-            if (movingLeft == true)
-            {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingLeft = false;
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingLeft = true;
-
-            }
-        }
+        
     }
 }
