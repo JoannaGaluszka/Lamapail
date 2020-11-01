28c1acf7893b3b2a9008876294a8fdd79adc33c7
Aminokwas <amadeusz.her@gmail.com>
2020-11-01 03:21:40 +0000

    lever script - toggle flip

--- Assets/Scripts/LeverScript.cs
+++ Assets/Scripts/LeverScript.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class LeverScript : MonoBehaviour
 {
     public bool Zmiana;
+    public bool is_flipped;
     Animator anim;
     void Start()
     {
         anim = GetComponent<Animator>();
+        is_flipped = false;
     }
 
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (is_flipped == false)
+            {
+                anim.SetBool("LeverFlip", true);
+                is_flipped = true;
+            }
+            else
+            {
+                anim.SetBool("LeverFlip", false);
+                is_flipped = false;
+            }
+        }
     }
     void OnTriggerStay2D()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        anim.SetBool("LeverFlip", true);
+        
+        
     }
 
 
