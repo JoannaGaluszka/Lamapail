2b2f56f8794ef947b6258e234f634c56317a2da0
JoannaGaluszka <jo.galuszka@gmail.com>
2020-11-02 20:17:56 +0100

    Switch

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject On;
+    public GameObject Off;
+
+    public bool isOn = false;
+
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = Off.GetComponent<SpriteRenderer>().sprite;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = On.GetComponent<SpriteRenderer>().sprite;
+
+        isOn = true;
+
+    }
+}
