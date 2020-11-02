00e7874b055d1e6529a7e34b37ba03d27ebbfcdf
JoannaGaluszka <jo.galuszka@gmail.com>
2020-11-02 23:19:29 +0100

    Colliders, script,

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,45 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float HP;
-    public float speed;
-    public float distance;
-    private bool movingLeft = true;
-    public Transform detection;
-    public GameObject BoinkEffect;
+	public float HP;
+	public float speed;
+	public bool MoveRight;
+	public float distance;
+	public Transform detection;
+	public GameObject BoinkEffect;
 
-    // private Animator Cycle       <-------------Przy gotowych spritach bedziemy kombinowac // dodać start przy animatorze
-    public GameObject Particle;
-   
-    void Update()
-    {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+	
+	void Update()
+	{
+		
+		if (MoveRight)
+		{
+			transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+			transform.localScale = new Vector2(-1, 1);
+		}
+		else
+		{
+			transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+			transform.localScale = new Vector2(1, 1);
+		}
+	}
+	void OnTriggerEnter2D(Collider2D trig)
+	{
+		if (trig.gameObject.CompareTag("turn"))
+		{
 
-        RaycastHit2D ground = Physics2D.Raycast(detection.position, Vector2.down, distance);
-        if (ground.collider == false)
-        {
-            if (movingLeft == true)
-            {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingLeft = false;
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingLeft = true;
-
-            }
-        }
-
-        if (HP <= 0)
-        {
-            Destroy(gameObject);
-        }
-
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-    }
+			if (MoveRight)
+			{
+				MoveRight = false;
+			}
+			else
+			{
+				MoveRight = true;
+			}
+		}
+	}
 }
