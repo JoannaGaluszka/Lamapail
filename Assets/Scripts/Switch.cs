fde45bba9c8b5dabdfbeafabc9e3981b6a0d7791
JoannaGaluszka <jo.galuszka@gmail.com>
2020-11-06 21:01:57 +0100

    Switch fixed

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
-    public GameObject On;
-    public GameObject Off;
+    public Sprite on, off;
 
     public bool isOn = false;
 
     private void Start()
     {
-        gameObject.GetComponent<SpriteRenderer>().sprite = Off.GetComponent<SpriteRenderer>().sprite;
+        gameObject.GetComponent<SpriteRenderer>().sprite = off;
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        gameObject.GetComponent<SpriteRenderer>().sprite = On.GetComponent<SpriteRenderer>().sprite;
+        gameObject.GetComponent<SpriteRenderer>().sprite = on;
 
         isOn = true;
+      
 
     }
 }
