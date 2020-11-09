0582cb543f52d19db3db7b2168adb190ca5708da
Aminokwas <amadeusz.her@gmail.com>
2020-11-09 23:02:53 +0100

    Prefab BouncingSlime z animacją (do optymalizacji)
    NIE MERGOWAĆ BEZSENSOWNIE BECAUSE I WORSHIP CHAOS I BEDZIE MESS

--- Assets/Scripts/BouncingSlime.cs
+++ Assets/Scripts/BouncingSlime.cs
@@ -6,10 +6,12 @@ public class BouncingSlime : MonoBehaviour
 {
     private CapsuleCollider2D CC2D;
     public float MocWyrzutu;
+    private Animator animatorek;
     // Start is called before the first frame update
     void Start()
     {
         CC2D = GetComponent<CapsuleCollider2D>();
+        animatorek = GetComponent<Animator>();
     }
 
 
@@ -17,6 +19,7 @@ public class BouncingSlime : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            animatorek.SetTrigger("ActivateBounce");
             collision.gameObject.GetComponent<Rigidbody2D>
                 ().AddForce(new Vector2(0f,MocWyrzutu), ForceMode2D.Impulse);
 
