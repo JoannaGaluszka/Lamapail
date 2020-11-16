80f2dab9d1c6086b715691f1b7a0dcdeb7f54834
Tony Hawk <ant.jastrzebski@gmail.com>
2020-11-16 13:03:11 +0100

    Revert "Revert "wip segregacja skryptów""
    This reverts commit 27f2e2b86423dcad7f22d4f8e5523fa121968208.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,26 +18,13 @@ public class PlayerController : MonoBehaviour
 
     private bool ground;
     private bool doubleJump;
-    //walljump 7.11 aj
-    public float WallCheckRadius;
-    private bool TouchingWall;
-    public Transform WallCheck;
-    private bool WallSlide;
-    public float WallSlideSpeed;
-    //++++
-    private bool WallJump;
-    public float WallForceHorizontal;
-    public float WallForceVertical;
-    public float WallJumpTime;
-
+    
     public int zycie;
     public int iloscSerc = 6;
 
     public Image[] heart;
     public Sprite full;
     public Sprite empty;
-
-    private bool didWallJump = false;
 
     private void Start()    
     {
@@ -81,24 +68,9 @@ public class PlayerController : MonoBehaviour
         }
         ground = Physics2D.OverlapCircle(feetPos.position, radius, whatIsGround);
 
-        /////////////////////////////
-        if (ground)
-        {
-            didWallJump = false;
-        }
-
         //chodzonko
         moveInput = Input.GetAxis("Horizontal");
-
-
-        if (didWallJump)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
-        } else
-        {
-            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-        }
-
+   
         if (moveInput > 0)
         {
             transform.eulerAngles = new Vector2(0, 0);
@@ -115,65 +87,14 @@ public class PlayerController : MonoBehaviour
             HeroAnimCont.SetBool("Idzie", false);
         }
 
-        /////////////////////////////////
-
         //skaczanko
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (ground)
             {
-                rb.velocity = Vector2.up * ForceJump;
-                //doubleJump = true;
-            }
-            //else
-            //{
-            //    if (doubleJump)
-            //    {
-            //        doubleJump = false;
-            //        rb.velocity = new Vector2(rb.velocity.x, 0);
-            //        rb.velocity = new Vector2(rb.velocity.x, ForceJump);
-            //    }
-            //}
-            
+                rb.velocity = Vector2.up * ForceJump;            
+            }          
         }
-        //wall Jump warunki
-        TouchingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, whatIsGround);
-
-        if(TouchingWall == true && ground == false && moveInput != 0)
-        {
-            WallSlide = true;
-        } else
-        {
-            WallSlide = false;
-        }
-        //zmiana charakterystyki rb przy slajdzie
-        if (WallSlide)
-        {                                                                    
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -WallSlideSpeed, float.MaxValue));
-           // Mathf.Clamp
-           // rb.velocity.y -= WallSlideSpeed;
-           // if (rb.velocity.y > float.MaxValue) {
-           //     rb.velocity.y = float.MaxValue;
-           // }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && WallSlide == true)
-        {
-            WallJump = true;
-            Invoke("WallJumpBlock", WallJumpTime);
-        }
-        //fizyka odbicia i think
-        if (WallJump == true)
-        {                                                    //tu z jakiegoś powodu przy odbiciu nie nadaje -moveinput, nie odbija i nie odwraca postaci/
-            didWallJump = true;
-            rb.velocity = new Vector2(WallForceHorizontal * -moveInput, WallForceVertical);
-            //rb.velocity = new Vector2(rb.velocity.x, WallForceVertical);
-            //rb.AddForce(new Vector2(WallForceHorizontal * -moveInput, 0), ForceMode2D.Impulse);
-            Debug.Log("odbicie");
-            //Debug.Log(-moveInput);
-           // Debug.Log(rb.velocity);
-        }
-
 
         if (zycie > iloscSerc)
         {
@@ -194,12 +115,6 @@ public class PlayerController : MonoBehaviour
     public void Damage(int obrazenia)
     {
         zycie -= obrazenia;
-    }
-
-    private void WallJumpBlock()
-    {
-        WallJump = false;
-        Debug.Log("WallJumpBlock");
     }
 }
 
---
+++ Assets/Scripts/WallJump.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJumpScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public LayerMask whatIsGround;
+
+    public float WallCheckRadius;
+    private bool TouchingWall;
+    public Transform WallCheck;
+    private bool WallSlide;
+    public float WallSlideSpeed;
+
+    private bool WallJump;
+    public float WallForceHorizontal;
+    public float WallForceVertical;
+    public float WallJumpTime;
+    private bool didWallJump = false;
+
+    void Update()
+    {
+        if (ground)
+        {
+            didWallJump = false;
+        }
+
+        if (didWallJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        }
+
+        TouchingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, whatIsGround);
+
+        if (TouchingWall == true && ground == false && moveInput != 0)
+        {
+            WallSlide = true;
+        }
+        else
+        {
+            WallSlide = false;
+        }
+        //zmiana charakterystyki rb przy slajdzie
+        if (WallSlide)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -WallSlideSpeed, float.MaxValue));
+            // Mathf.Clamp
+            // rb.velocity.y -= WallSlideSpeed;
+            // if (rb.velocity.y > float.MaxValue) {
+            //     rb.velocity.y = float.MaxValue;
+            // }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && WallSlide == true)
+        {
+            WallJump = true;
+            Invoke("WallJumpBlock", WallJumpTime);
+        }
+        //fizyka odbicia i think
+        if (WallJump == true)
+        {                                                    //tu z jakiegoś powodu przy odbiciu nie nadaje -moveinput, nie odbija i nie odwraca postaci/
+            didWallJump = true;
+            rb.velocity = new Vector2(WallForceHorizontal * -moveInput, WallForceVertical);
+            //rb.velocity = new Vector2(rb.velocity.x, WallForceVertical);
+            //rb.AddForce(new Vector2(WallForceHorizontal * -moveInput, 0), ForceMode2D.Impulse);
+            Debug.Log("odbicie");
+            //Debug.Log(-moveInput);
+            // Debug.Log(rb.velocity);
+        }
+    }
+    private void WallJumpBlock()
+    {
+        WallJump = false;
+        Debug.Log("WallJumpBlock");
+    }
+}
