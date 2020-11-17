e5742211ef94814057b7ff14b33b0cfe12fe9fe6
Tony Hawk <ant.jastrzebski@gmail.com>
2020-11-17 04:44:54 +0100

    kradne od Amadiego

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -3,50 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class FallingPlatform : MonoBehaviour
 
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
     Vector2 startPos;
-    public SpriteRenderer Spr;
+    private SpriteRenderer Spr;
     private BoxCollider2D coll;
-
-    // Start is called before the first frame update
+    public float CzasWywołania;
+    public float RespawnTime;
     void Start()
     {
         coll = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         Spr = GetComponent<SpriteRenderer>();
         startPos = transform.position;
-    }
 
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player") 
+        if (collision.gameObject.CompareTag("Player"))  
         {
-            Invoke("FallingPadzik", 0.4f);
+            Invoke("FallingPad", CzasWywołania);
         }
     }
-    void FallingPadzik()
-    {
-        rb.isKinematic = false;
-        Spr.sortingLayerName = "Mid";
-        Invoke("ColliderOFF", 0.15f);
-        Invoke("Respawn", 3.75f);
-        
-        
+    void FallingPad()
+     {
+     rb.isKinematic = false;
+     Spr.sortingLayerName = "Mid";
+     Invoke("ColliderOFF", 0.15f);
+     Invoke("Respawn", RespawnTime);
     }
     void ColliderOFF()
     {
-        coll.enabled = false;
+     coll.enabled = false;
     }
     void Respawn()
     {
-        transform.position = startPos;
-        Spr.sortingLayerName = "Platforms";
-        rb.isKinematic = true;
-        rb.velocity = new Vector2(0, 0);
-        coll.enabled = true;
-    }
+       transform.position = startPos;
+       Spr.sortingLayerName = "Platforms";
+       rb.isKinematic = true;
+       rb.velocity = new Vector2(0, 0);
+       coll.enabled = true;
+     }
 
 }
