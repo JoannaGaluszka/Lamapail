00f57e55e6b62cfbf180f492d9a409d8968a61f3
JoannaGaluszka <jo.galuszka@gmail.com>
2020-11-20 20:24:21 +0100

    Spadajacce kolce. Kolce znikaja po zetknieciu sie z TAGIEM "Ground"!!

---
+++ Assets/Scripts/FallingObjects.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjects : MonoBehaviour
+{
+    Rigidbody2D rb;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            rb.isKinematic = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+           
+            DestroyFallingObject();
+
+        }
+        else
+        {
+            if (other.gameObject.CompareTag("Ground"))
+                Destroy(gameObject);
+        }
+
+        
+       
+    }
+    
+    void DestroyFallingObject()
+    {
+            Destroy(gameObject);
+    }
+
+}
