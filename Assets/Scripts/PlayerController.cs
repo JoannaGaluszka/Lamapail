458c65632bc41870239de96ccf0460d9b0f27942
Aminokwas <amadeusz.her@gmail.com>
2020-11-24 02:17:48 +0100

    Dodany Prefab MovingPlatform, Dodany skrypt StickToPlatform, wszystko na scenie AMI (żadnych zmian w playercontrollerze)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -179,6 +179,7 @@ public class PlayerController : MonoBehaviour
         WallJump = false;
         Debug.Log("WallJumpBlock");
     }
+
 }
 
 
---
+++ Assets/Scripts/StickToPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickToPlatform : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name.Equals("MovingPlatform"))
+            this.transform.parent = col.transform;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.name.Equals("MovingPlatform"))
+            this.transform.parent = null;
+    }
+}
