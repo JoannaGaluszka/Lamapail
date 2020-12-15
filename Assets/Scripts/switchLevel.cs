9680f86262870c072846658fc2130fd74b8d3d3a
JoannaGaluszka <jo.galuszka@gmail.com>
2020-12-15 02:11:46 +0100

    next level po zebraniu diaxa

---
+++ Assets/Scripts/switchLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class switchLevel : MonoBehaviour
+{
+    public int index;
+    public string nextLevel;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        SceneManager.LoadScene(nextLevel);
+    }
+
+}
