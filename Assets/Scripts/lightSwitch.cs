066f38b63718d60aee10c3bfa56cbc1566e2deaf
jonhonesy <arkadiuszjaroszynskimrc@gmail.com>
2021-01-18 20:35:51 +0100

    Dodatki wyśmietnite, sorki jak walnie

---
+++ Assets/Scripts/lightSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+
+public class lightSwitch : MonoBehaviour
+{
+    Light2D lamp;
+    Color color0 = Color.red;
+    Color color1 = Color.green;
+    public bool TriggerEntered;
+
+    void Start()
+    {
+        lamp = this.gameObject.GetComponent<Light2D>();
+        Debug.Log(lamp);
+        lamp.color = color0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TriggerEntered = true;
+    }
+
+    void Update()
+    {
+        if (TriggerEntered == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                lamp.color = color1;
+        }
+    }
+}
