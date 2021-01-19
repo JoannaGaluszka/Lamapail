bc6f3e9e685927537e118689b416a7e636d1af0e
Tony Hawk <ant.jastrzebski@gmail.com>
2021-01-19 03:14:53 +0100

    naprawa ognicha
    1. naprawiłem dźwięk ognicha, żeby odtworzył się tylko raz niezależnie od ilości kolizji z graczem (w kodzie)
    2. naprawilem zmiane światła ognicha (w kodzie)
    3. jakieś pierdoły

--- Assets/Scripts/LightTrigger.cs
+++ Assets/Scripts/LightTrigger.cs
@@ -8,6 +8,7 @@ public class LightTrigger : MonoBehaviour
     Light2D lamp;
     Color color0 = Color.black;
     Color color1 = Color.yellow;
+    private bool Ognicho = false;
     void Start()
     {
         lamp = this.gameObject.GetComponent<Light2D>();
@@ -15,8 +16,17 @@ public class LightTrigger : MonoBehaviour
         lamp.color = color0;
     }
 
-    void OnTriggerStay2D()
+    void Update()
     {
-        lamp.color = color1;
+        if(Ognicho == true)
+            lamp.color = color1;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Ognicho = true;
+        }
     }
 }
